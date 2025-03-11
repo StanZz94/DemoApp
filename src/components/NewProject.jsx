51efd943185911e9be2db8f056600f2b750a37ea
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2025-03-12 01:07:30 +0200

    create NewProject component

---
+++ src/components/NewProject.jsx
@@ -0,0 +1,23 @@
+export default function NewProject() {
+
+    return <div>
+        <menu>
+            <li>Cancel</li>
+            <li>Save</li>
+        </menu>
+        <div>
+            <p>
+                <label>Title</label>
+                <input type="text" name="" id="" />
+            </p>
+            <p>
+                <label>Description</label>
+                <textarea></textarea>
+            </p>
+            <p>
+                <label>Due Date</label>
+                <input type="text" name="" id="" />
+            </p>
+        </div>
+    </div>
+}
