da417c784bb08c1ee4a4e5cad930cc07be8cb90a
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2025-03-12 03:33:04 +0200

    create custon button component and replace it

--- src/components/NoProjectSelected.jsx
+++ src/components/NoProjectSelected.jsx
@@ -1,4 +1,5 @@
 import noProjectImage from '../assets/no-project.png'
+import Button from './Button'
 
 export default function NoProjectSelected() {
 
@@ -6,6 +7,6 @@ export default function NoProjectSelected() {
         <img className="w-16 h-16 object-contain mx-auto" src={noProjectImage} alt='missing image' />
         <h2 className="text-xl font-bold text-stone-400 my-4">No Project Selected!</h2>
         <p className="text-stone-400 mb-4">Select a project or start a new one.</p>
-        <p className="mt-8"><button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Create new project</button></p>
+        <p className="mt-8"><Button>Create new project</Button></p>
     </div>
 }
