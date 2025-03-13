76a0d2a1e0ed74922bb613b1d3275e0c94ec145c
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2025-03-13 23:29:59 +0200

    create refs to NewProject inputs and collect data from them

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,17 +1,28 @@
+import { useRef } from "react";
 import Button from "./Button";
 import Input from "./Input";
 
 export default function NewProject() {
 
+    const title = useRef();
+    const description = useRef();
+    const dueDate = useRef();
+    
+    function handleSave() {
+        const enteredTitle = title.current.value;
+        const enteredDescription = description.current.value;
+        const enteredDueDate = dueDate.current.value;
+    }
+
     return <div className="w-[35rem] mt-16 ">
         <menu className="flex items-center justify-end gap-4 my-4">
             <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
-            <li><Button>Save</Button></li>
+            <li><Button onClick={handleSave}>Save</Button></li>
         </menu>
         <div>
-            <Input label='Title' />
-            <Input label='Description' textarea />
-            <Input label='Due Date' />
+            <Input ref={title} label='Title' />
+            <Input ref={description} label='Description' textarea />
+            <Input ref={dueDate} label='Due Date' />
         </div>
     </div>
 }
