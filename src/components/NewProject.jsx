79749b856e41fa311c20872cd56b8e2557128c28
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2025-03-14 01:43:06 +0200

    add cancel on NewProject

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -3,7 +3,7 @@ import Button from "./Button";
 import Input from "./Input";
 import Modal from "./Modal";
 
-export default function NewProject({ onAdd }) {
+export default function NewProject({ onAdd, onCancel }) {
     const modal = useRef();
     const title = useRef();
     const description = useRef();
@@ -26,6 +26,10 @@ export default function NewProject({ onAdd }) {
         })
     }
 
+    function handleCancel() {
+        onCancel();
+    }
+
     return <>
     <Modal ref={modal} buttonCaption='Close'>
         <h2 className="text-xl font-bold text-stone-800 my-4">Invalid Input!</h2>
@@ -34,7 +38,7 @@ export default function NewProject({ onAdd }) {
     </Modal>
         <div className="w-[35rem] mt-16 ">
             <menu className="flex items-center justify-end gap-4 my-4">
-                <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
+                <li><button onClick={onCancel} className="text-stone-800 hover:text-stone-950">Cancel</button></li>
                 <li><Button onClick={handleSave}>Save</Button></li>
             </menu>
             <div>
