121fc73ab3f5510373bfc3a794faf40a2e98e810
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2025-03-16 22:29:52 +0200

    add change handler

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -1,7 +1,15 @@
+import { useState } from "react"
+
 export default function NewTask() {
+    
+    const [enteredTask, setEnteredTask] = useState();
+
+    function handleChange(e) {
+        setEnteredTask(e.target.value);
+    }
 
     return <div className="flex items-center gap-4">
-        <input type='text' className="w-64 px2 py-1 rounded-sm bg-stone-200" />
+        <input onChange={handleChange} value={enteredTask} type='text' className="w-64 px2 py-1 rounded-sm bg-stone-200" />
         <button className="text-stone-700 hover:text-stone-950">Add Task</button>
     </div>
 }
