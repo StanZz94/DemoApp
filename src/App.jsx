39639be0c4b4cb7905d76d80c3e0fd26e09a732b
StanZz94 <123236911+StanZz94@users.noreply.github.com>
2025-03-17 23:30:40 +0200

    fix active project in siderbar

--- src/App.jsx
+++ src/App.jsx
@@ -108,7 +108,7 @@ function App() {
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar projects={projectsState.projects} onStartAddProject={handleStartAddProject} onSelectProject={handleSelectProject} />
+      <ProjectsSidebar selectedProjectId={projectsState.selectedProjectId} projects={projectsState.projects} onStartAddProject={handleStartAddProject} onSelectProject={handleSelectProject} />
       {content}
     </main>
   );
